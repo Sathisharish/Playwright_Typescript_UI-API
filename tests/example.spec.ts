cef8d6ef80b932286046064e1a7caad40d0de871
git config username <sathishkumar.ravi@aspiresys.com>
2026-05-06 18:33:06 +0530

    Commented  test in spec file

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -39,7 +39,7 @@ test('get started link duplicate', async ({ page }) => {
 
 });
 
-test('get started link', async ({ page }) => {
+/*test('get started link', async ({ page }) => {
 
   await page.goto('https://playwright.dev/');
 
@@ -49,4 +49,4 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 
-});
+}); */
